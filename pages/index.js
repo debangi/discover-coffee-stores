99c332fd9e64b8fc582d2ef350f51da32ebef61a
debangi <debangiofficial@gmail.com>
2024-06-05 13:26:50 +0530

    banner component added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Banner from '@/components/Banner';
+
 export default function Home() {
+  const handleOnBannerBtnClick = () => {
+    console.log('Hi banner button');
+  };
   console.log('styles', styles);
   return (
     <div className={styles.container}>
@@ -11,7 +16,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Coffee Connoisseur</h1>
+        <Banner
+          buttonText='View stores nearby'
+          handleOnClick={handleOnBannerBtnClick}
+        />
       </main>
     </div>
   );
