9992d8c94c247a054c297abb1bf434a1480d4799
debangi <debangiofficial@gmail.com>
2024-06-10 16:07:25 +0530

    added card styles

--- components/card.js
+++ components/card.js
@@ -1,13 +1,25 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import Styles from './card.module.css';
+import styles from './card.module.css';
+import cls from 'classnames';
 
 const Card = (props) => {
   return (
-    <Link href={props.href} legacyBehavior>
-      <div>
-        <h2>{props.name}</h2>
-        <Image src={props.imgUrl} width={260} height={160} />
+    <Link href={props.href}>
+      <div className={styles.cardLink}>
+        <div className={cls('glass', styles.container)}>
+          <div>
+            <h2 className={styles.cardHeaderWrapper}>{props.name}</h2>
+          </div>
+          <div className={styles.cardImageWrapper}>
+            <Image
+              className={styles.cardImage}
+              src={props.imgUrl}
+              width={200}
+              height={160}
+            />
+          </div>
+        </div>
       </div>
     </Link>
   );
