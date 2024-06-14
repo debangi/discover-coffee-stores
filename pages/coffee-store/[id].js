2cee4feca60d97a643d516984d77bff0586bc91e
debangi <debangiofficial@gmail.com>
2024-06-14 19:55:12 +0530

    added geolocation api and fetched data with user location

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -11,11 +11,13 @@ export async function getStaticProps(staticProps) {
   const params = staticProps.params;
   const coffeeStores = await fetchCoffeeStores();
 
+  const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
+    return coffeeStore.id.toString() === params.id;
+  });
+
   return {
     props: {
-      coffeeStore: coffeeStores.find((coffeeStore) => {
-        return coffeeStore.id.toString() === params.id;
-      }),
+      coffeeStore: findCoffeeStoreById ? findCoffeeStoreById : {},
     },
   };
 }
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import Card from '@/components/card';
 import { fetchCoffeeStores } from '@/lib/coffee-stores';
 import useTrackLocation from '@/hooks/use-track-location';
+import { useEffect, useState } from 'react';
 
 export async function getStaticProps(context) {
   const coffeeStores = await fetchCoffeeStores();
@@ -14,10 +15,31 @@ export async function getStaticProps(context) {
 }
 
 export default function Home(props) {
-  console.log('props', props);
+  // console.log('props', props);
   const { handleTrackLocation, latLong, locationErrorMsg, isFindingLocation } =
     useTrackLocation();
+
+  const [coffeeStores, setCoffeeStores] = useState('');
+  const [coffeeStoresError, setCoffeeStoresError] = useState(null);
   console.log({ latLong, locationErrorMsg });
+
+  useEffect(() => {
+    async function setCoffeeStoresByLocation() {
+      if (latLong) {
+        try {
+          const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 20);
+          console.log({ fetchedCoffeeStores });
+          setCoffeeStores(fetchedCoffeeStores);
+        } catch (error) {
+          // console.log({ error });
+          setCoffeeStoresError(error.message);
+        }
+      }
+    }
+
+    setCoffeeStoresByLocation();
+  }, [latLong]);
+
   const handleOnBannerBtnClick = () => {
     console.log('Hi banner button');
     handleTrackLocation();
@@ -35,7 +57,10 @@ export default function Home(props) {
           buttonText={isFindingLocation ? 'Locating...' : 'View stores nearby'}
           handleOnClick={handleOnBannerBtnClick}
         />
+
         {locationErrorMsg && <p>Something went wrong : {locationErrorMsg}</p>}
+        {coffeeStoresError && <p>Something went wrong : {coffeeStoresError}</p>}
+
         <div className={styles.heroImage}>
           <Image
             src='/static/hero-img.png'
@@ -44,6 +69,29 @@ export default function Home(props) {
             alt='hero'
           />
         </div>
+
+        {coffeeStores.length > 0 && (
+          <div className={styles.sectionWrapper}>
+            <h2 className={styles.heading2}>Stores Near Me</h2>
+            <div className={styles.cardLayout}>
+              {coffeeStores.map((coffeeStore) => {
+                return (
+                  <Card
+                    key={coffeeStore.id}
+                    name={coffeeStore.name}
+                    imgUrl={
+                      coffeeStore.imgUrl ||
+                      'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+                    }
+                    href={`/coffee-store/${coffeeStore.id}`}
+                    className={styles.card}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        )}
+
         {props.coffeeStores.length > 0 && (
           <div className={styles.sectionWrapper}>
             <h2 className={styles.heading2}>Toronto stores</h2>
